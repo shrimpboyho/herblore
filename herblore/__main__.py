012a175d4c8b1d1bac75e20bc55d9d563d31f8e1
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-09-30 17:46:06 -0400

    Added better player coordinate system (cartesian) and set up teleport system

--- herblore/__main__.py
+++ herblore/__main__.py
@@ -32,14 +32,24 @@ keys_held = []
 def on_key_press(symbol, modifiers):
     global first_f1_hit
     keys_held.append(symbol)
+    
+    # Fullscreen toggle f1 key
     if symbol == key.F1:
         if first_f1_hit == False:
 	    window.set_fullscreen(True)
+	    player_sprite.x = window.width/2
+            player_sprite.y = window.height/2
+	    terrain_sprite.x = window.width/2
+            terrain_sprite.y = window.height/2
 	    first_f1_hit = True
 	else:
 	    window.set_fullscreen(False)
+	    player_sprite.x = window.width/2
+            player_sprite.y = window.height/2
+	    terrain_sprite.x = window.width/2
+            terrain_sprite.y = window.height/2
 	    first_f1_hit = False
-
+    
 @window.event
 def on_key_release(symbol, modifiers):
     keys_held.pop(keys_held.index(symbol))      
@@ -48,16 +58,20 @@ def on_key_release(symbol, modifiers):
 @window.event
 def on_mouse_press(x, y, button, modifiers):
     if button == mouse.LEFT:
-        print 'The left mouse button was pressed.'
+        print 'The left mouse button was pressed at (' + str(x) + ','+ str(y) + ')'
+	print("Player coordinates:" + str(current_player.coordX) + ',' + str(current_player.coordY))
+        print("Map coordinates:" + str(terrain_sprite.x) + ',' + str(terrain_sprite.y))
     if button == mouse.RIGHT:
-        print 'The right mouse button was pressed.'
+        print 'The right mouse button was pressed at (' + str(x) + ','+ str(y) + ')'
 
 # Set up background map
 terrain_image = pyglet.image.load("herblore/assets/sprites/map.png")
+terrain_image.anchor_x = terrain_image.width / 2
+terrain_image.anchor_y = terrain_image.height / 2
 terrain_sprite =  pyglet.sprite.Sprite(terrain_image)
 
 # Create a player object
-current_player = Player("jewishBoy", "Dire", "Lina")
+current_player = Player("jewishBoy", "Radiant", "Lina")
 
 # Create the custom player HUD stuff
 player_name_label = pyglet.text.Label(current_player.username,
@@ -71,36 +85,38 @@ player_level_label = pyglet.text.Label("Level " + str(current_player.level),
 
 # Set up player sprites
 player_image = pyglet.resource.image("assets/sprites/player.png")
+player_image.anchor_x = player_image.width / 2
+player_image.anchor_y = player_image.height / 2
 player_sprite = pyglet.sprite.Sprite(player_image)
 player_sprite.scale = 0.2
 
-# Set up player spawn point based on team
+# Set up player in center of the screen
+player_sprite.x = window.width/2
+player_sprite.y = window.height/2
+
+# Draw the map
+terrain_sprite.x = window.width/2
+terrain_sprite.y = window.height/2
+
 if(current_player.team == "Dire"):
-    player_sprite.x = 330.15
-    player_sprite.y = 250.15
-    terrain_sprite.x = -830.15
-    terrain_sprite.y = -830.15
-
+    current_player.teleport(182,164,terrain_sprite,window)
 if(current_player.team == "Radiant"):
-    player_sprite.x = 316.65
-    player_sprite.y = 237.9
-    terrain_sprite.x = 316.65
-    terrain_sprite.y = 237.9
+    current_player.teleport(-182,-164,terrain_sprite,window)
 
-# Update function called all the time
+# Update function called all the time to handle input
 def update(interval):
-    if key.W in keys_held and player_sprite.y < 250.75:
-        player_sprite.y += .05
-	terrain_sprite.y -= 4
-    if key.S in keys_held and player_sprite.y > 237.25:
-        player_sprite.y -= .05
-	terrain_sprite.y += 4
-    if key.A in keys_held and player_sprite.x > 316.1:
-        player_sprite.x -= .05
-	terrain_sprite.x += 4
-    if key.D in keys_held and player_sprite.x < 330.55:
-        player_sprite.x += .05
-	terrain_sprite.x -= 4
+    if key.W in keys_held:
+	current_player.coordY += 1
+	terrain_sprite.y -= 3
+    if key.S in keys_held:
+	current_player.coordY -= 1
+	terrain_sprite.y += 3
+    if key.A in keys_held:
+	current_player.coordX -= 1    
+	terrain_sprite.x += 3
+    if key.D in keys_held:
+	current_player.coordX += 1
+	terrain_sprite.x -= 3
 
 # Set up interval
 clock.schedule_interval(update, .01)
@@ -120,8 +136,6 @@ def on_draw():
     # Draw the HUD
     player_name_label.draw()
     player_level_label.draw()
-
-    #print(str(player_sprite.x) + ',' + str(player_sprite.y))
 
 # Begin application
 pyglet.app.run()
